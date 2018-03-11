67be63c3fee421dbf37c2e6a3bb34c672d12a61d
Tan loc <johndoe@example.com>
2018-03-11 15:48:59 +0700

    commit create project BE

---
+++ OxfordEnglishAPI/OxfordEnglish/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OxfordEnglish.Paramaters;
+
+namespace OxfordEnglish.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Vocabulary value)
+        {
+            var a = 1;
+
+        }
+    }
+}
